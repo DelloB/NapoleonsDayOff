de33fa283717a920d676679e02cde75c05afde3a
DelloB <david.bello@students.plymouth.ac.uk>
2025-04-04 14:57:15 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # NapoleonsDayOff
+
+NAPOLEON'S DAY OFF
+
+This is a game where you must make choices to maximize your charisma as well as your combat power for the battle of waterloo
+You have 5 minutes to make choices and you must end the day on your own before the 5 minutes are up otherwise you'll miss the battle and your army will
+lose by default. Good luck!
+
+Characters
+
+The main character of this game is Napoleon, almost a 1 to 1 on this historical figure, but you assume the role of Napoleon, so it influences the choices you are able to make. Napoleon is a French military leader and emperor. He was short, ambitious, and a strong leader. 
+
+Louis-Alexandre Berthier is a French military commander and second hand man of Napoleon, he acts as a voice prompting Napoleon to make decisions in the game. He is organised, meticulous and relentlessly loyal which is reflected in his dialogue. 
+
+ Story
+Initially, the story was Napoleon, being the workaholic he is, decides to take some free leisure time off in order to clear his mind in preparation for the battle of Waterloo. The story then pans to his second in command (Louis), who has seen that Napoleon misplaced his watch and Charleville M1777 musket. The player would have then had to make decisions based on their knowledge of Napoleon as well as clues provided in-game in order to find Napoleon and provide him with these items before he misses the battle. The Story was later changed to be Napoleon needing to optimize the last bit of free time he has left in order to have sufficient combat power and charisma, and leader level for the battle of Waterloo, the endings vary from what the values of each of these are.
+Theme
+This game plays with the idea of having a good work-life balance and spending your time wisely. There’s also a strong military theme overseeing the whole game and whilst not being a comedy in genre there are little hints of comedy in the games choices as well, other than this it’s a relatively serious fictional story based on real life events.
+
+
+
+Story Progression
+The Game starts with Napoleon’s second in command asking what he would like to do, then the player is given an array of choices with multiple different outcomes and multiple different effects on his stats. The story progresses within real life time, in the event the player doesn’t end the time within 5 minutes then it result in a guaranteed bad end, there is an ending where the player has sufficient Charisma but insufficient Combat power, the inverse of this and finally an ending where the player meets all requirements which will be the true ending for this story.
+Gameplay
+The goal of this game is to make the right decisions within the given time frame to maximise both Combat power and Charisma.
+
+Skills that the user will utilize playing include critical thinking, thinking under pressure, memory, puzzle solving, management of resources, strategy, time management, and logical reasoning.
+
+The game mechanics include scores and levels, which act as a trackable, readable metric for the player to understand how they are impacting the game and its story. Point and click as the method of how different things in the game are done. Dialogue systems, where players choose from a set of action/dialogue options to influence the game. Player choice and consequences, and statistics at the end of the story, where the player’s choices impact the story, and this is portrayed.
+
+The challenge comes from managing how different options influence your stats differently and keeping both values up whilst under pressure as a result of time constraints, the having to manage time yourself and not having it being done automatically for you adds another layer to the challenge since you always have to be thinking about the time you’re spending whilst thinking about other things and making choices constantly. The progression is dynamic as it’s up to you. And depending on when you choose to progress, or in this case, finish it, can affect the outcome of the story.
+
+Whilst there is no direct way to lose, losing could be interpreted as getting a bad ending, and the conditions for losing is not having enough Charisma, not enough Combat power, or not finishing up before the time allocated runs out.
+
+The artstyle should be french themed in colour scheme as it’s about a French historical figure and feature many military like imagery throughout it all, perhaps a classical or stylised artstyle accompanying the menus and interfaces. 
+
+The music and sound effects aim to primarily make clicking through each option satisfying and making it clear with sound design what you’re doing when clicking each button. The background music also reflects how much time is left, helps add tension, and improves clarity for the player. Ideally, the music would be on theme for the semi-serious war-like theme the game presents. Different sound effects for options giving you more combat power or more charisma could be added to help with clarity, also. 
--- game.js
+++ game.js
@@ -25,7 +25,7 @@ const story = {
             { text: "Resource Gathering (Gathering)", next: "gathering", art: "c.jpg" },
             { text: "Army Inspection (Inspection)", next: "inspection", art: "d.jpg" }
         ],
-        art: "daystart.jpg"
+        art: "Oni.png"
     },
     "leading": {
         text: "You Enter your private quarters, How will you hone your Leadership for the next battle?",
@@ -34,28 +34,28 @@ const story = {
             { text: "Rehearse performances and speeches for soldier morale", next: "leadingspeech", CharismaChange: getRandomScore(), Combat_StrengthChange: -2 },
             { text: "Examine and embrace any possible new technologies to get a competitive edge", next: "leadingcompetitive", CharismaChange: 1, Combat_StrengthChange: 1 }
         ],
-        art: "book.jpg"
+        art: "Twi.png"
     },
     "leadingreflect": {
-        text: "You think back to a battle where one of your miscalulations cost you deeply.",
+        text: "You think back to a battle where one of your miscalculations cost you deeply.",
         choices: [
             { text: "Scrutinize your thought process and hone your critical thinking", next: "chapterEnd", CharismaChange: -5, Combat_StrengthChange: getRandomScore() }
         ],
-        art: "reflectBook.jpg"
+        art: "foui.png"
     },
     "leadingspeech": {
         text: "You exercise your theatrical flair and explore several techniques to ensure impact in your future speeches.",
         choices: [
             { text: "Work on careful composition, strategic delivery and visual spectacle", next: "chapterEnd", CharismaChange: getRandomScore(), Combat_StrengthChange: -5 }
         ],
-        art: "speechBook.jpg"
+        art: "fivi.png"
     },
     "leadingcompetitive": {
         text: "You pressure your scientists and researchers to make many breakthroughs in your army's tools.",
         choices: [
             { text: "Take and apply these new findings", next: "chapterEnd", CharismaChange: getRandomScore(), Combat_StrengthChange: getRandomScore() }
         ],
-        art: "competitiveBook.jpg"
+        art: "sii.png"
     },
     "combat": {
         text: "You head to the training fields yourself. What combat aspect will you focus on?",
@@ -64,28 +64,28 @@ const story = {
             { text: "Discipline and Morale", next: "combatDandM", CharismaChange: 18, Combat_StrengthChange: -4 },
             { text: "Strength and Stamina", next: "combatStrengthandStam", CharismaChange: 0, Combat_StrengthChange: getRandomScore() }
         ],
-        art: "game.jpg"
+        art: "sevei.png"
     },
     "combatSpeed": {
         text: "You extensively practice marching, sidestepping and sprinting until you're exhausted.",
         choices: [
             { text: "Rest and let your legs heal and improve", next: "chapterEnd", CharismaChange: -1, Combat_StrengthChange: 5 }
         ],
-        art: "SpeedGame.jpg"
+        art: "twelvi.png"
     },
     "combatDandM": {
         text: "You undergo a strict and rigerous routine encompassing all the things that a leader must be adept in.",
         choices: [
             { text: "Repeat the routine over and over seeking perfection.", next: "chapterEnd", CharismaChange: 8, Combat_StrengthChange: -7 }
         ],
-        art: "DandMGame.jpg"
+        art: "thirteei.png"
     },
     "combatStrengthandStam": {
         text: "You do some simple weight training.",
         choices: [
             { text: "Go for a steady 5 laps around the training grounds.", next: "chapterEnd", CharismaChange: -6, Combat_StrengthChange: getRandomScore() }
         ],
-        art: "StrengthandStamGame.jpg"
+        art: "fifteei.png"
     },
     "gathering": {
         text: "You are preparing a team to further increase your overall resources. What do you focus on?",
@@ -94,28 +94,28 @@ const story = {
             { text: "Material and Financial Resources", next: "gatheringMatsandFinance", CharismaChange: 1, Combat_StrengthChange: 1 },
             { text: "Logistical and Technical Resources", next: "gatheringLogAndTech", CharismaChange: 8, Combat_StrengthChange: 7 }
         ],
-        art: "gathering.jpg"
+        art: "fourteei.png"
     },
     "gatheringHR": {
         text: "You maintain your elite units and distribute propaganda.",
         choices: [
             { text: "Finish up any training and or recuritment.", next: "chapterEnd", CharismaChange: 15, Combat_StrengthChange: -10 }
         ],
-        art: "HRgathering.jpg"
+        art: "sebn.png"
     },
     "gatheringMatsandFinance": {
         text: "You expand your arsenal of cannons, muskets and other weapons as well as ordering your supplies for your army.",
         choices: [
             { text: "Go over your supply chains again to maximise efficiency", next: "chapterEnd", CharismaChange: getRandomScore(), Combat_StrengthChange: getRandomScore() }
         ],
-        art: "MatsandFinancegathering.jpg"
+        art: "nini.png"
     },
     "gatheringLogAndTech": {
         text: "You revolutionise your logistical systems and give out some of your newer technology to more of your army.",
         choices: [
             { text: "Teach the affected parties how to use the new tools they have been provided.", next: "chapterEnd", CharismaChange: -5, Combat_StrengthChange: getRandomScore() }
         ],
-        art: "LogAndTechgathering.jpg"
+        art: "eighi.png"
     },
     "inspection": {
         text: "You head over to your Army Barracks, what do you do?",
@@ -124,28 +124,28 @@ const story = {
             { text: "Look through the Army's weapons, calvary and rations to make sure they are all up to a good standard", next: "inspectionWeapons", CharismaChange: 0, Combat_StrengthChange: 5 },
             { text: "Test the Army's Loyalty and look out for any double agents or spies", next: "inspectionSpy", CharismaChange: 3, Combat_StrengthChange: 3 },
         ],
-        art: "inspection.jpg"
+        art: "ight.png"
     },
     "inspectionHeadcount": {
         text: "You celebrate previous battles with your comrades and become more familiar with the numbers on your side.",
         choices: [
             { text: "Clean up in another group bonding activity", next: "chapterEnd", CharismaChange: 5, Combat_StrengthChange: 5 }
         ],
-        art: "Headcountinspection.jpg"
+        art: "ighttt.png"
     },
     "inspectionWeapons": {
         text: "You assemble your quality control team and go through everything to make sure there are as many funcitonal tools at your disposal as possible.",
         choices: [
             { text: "Continue inspection", next: "chapterEnd", CharismaChange: 5, Combat_StrengthChange: 5 }
         ],
-        art: "Weaponsinspection.jpg"
+        art: "wpo.png"
     },
     "inspectionSpy": {
         text: "You employ tactics such as incentives and more to sniff out any possible rats in your army.",
         choices: [
             { text: "TEST YOUR ARMY'S LOYALTY", next: "chapterEnd", CharismaChange: 5, Combat_StrengthChange: 5 }
         ],
-        art: "Spyinspection.jpg"
+        art: "rats.png"
     },
     "chapterEnd": {
         text: "You finish up what you were doing. How do you proceed?",
@@ -153,7 +153,7 @@ const story = {
             { text: "Choose another activity", next: "start", scoreChange: 0, Combat_StrengthChange: 0 },
             { text: "End the day", next: "end", scoreChange: 0, Combat_StrengthChange: 0 }
         ],
-        art: "dayend.jpg"
+        art: "finnn.png"
     },
     "end": {
         text: "Your day of preperation comes to an end. Time to reflect.",
